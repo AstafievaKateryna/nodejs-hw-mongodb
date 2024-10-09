187895b5621d88fb9241807f097a09c9f3d33ed2
AstafievaKateryna <134802379+AstafievaKateryna@users.noreply.github.com>
2024-10-09 22:18:06 +0300

    final

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,15 +1,10 @@
 export const ENV_VARS = {
   PORT: 'PORT',
+};
+
+export const MONGO_DB_VARS = {
   MONGODB_USER: 'MONGODB_USER',
   MONGODB_PASSWORD: 'MONGODB_PASSWORD',
   MONGODB_URL: 'MONGODB_URL',
   MONGODB_DB: 'MONGODB_DB',
 };
-
-export const SORT_ORDER = {
-  ASC: 'asc',
-  DESC: 'desc',
-};
-
-export const FIFTEEN_MINUTES = 15 * 60 * 1000;
-export const ONE_DAY = 24 * 60 * 60 * 1000;
